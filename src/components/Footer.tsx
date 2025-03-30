9b264d74e79cff910edfb9c2599676d0e142c857
inarey <thoselovelywords@gmail.com>
2025-03-31 06:52:18 +0900

    modified the footer and hashtags css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return <>
   <footer className="footer">
     <small>
-    <p>&copy; Copyright by <a href="https.bytegrad.com" target="_blank">ByteGrad</a></p>
+    <p>&copy; Copyright by <a href="https.bytegrad.com" target="_blank">ByteGrad.com</a></p>
     <p>Code along by <span className="u-bold u-italic">Shan-Ti</span></p>
     </small>
   </footer>
